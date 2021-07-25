4fa87d459a2288e1279eb57df5ca220c87fb7dcd
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2021-07-25 21:57:35 +0200

    Update to warning URLs

--- demo_data/urls.sh
+++
@@ -1,8 +0,0 @@
-curl https://warnung.bund.de/bbk.mowas/gefahrendurchsagen.json >gefahrendurchsagen.json
-curl https://warnung.bund.de/bbk.dwd/unwetter.json >unwetter.json
-curl https://warnung.bund.de/bbk.lhp/hochwassermeldungen.json >hochwassermeldungen.json
-
-curl https://warnung.bund.de/bbk.dwd/waldbrand.json >waldbrand.json
-https://warnung.bund.de/bbk.bgr/erdbeben.json
-
-https://warnung.bund.de/bbk.wsv/hochwasser.json
