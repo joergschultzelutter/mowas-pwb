844ff855d6d21ad42f9aff06534d5830515dd515
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2021-08-16 16:20:12 +0200

    Add files via upload

--- src/mowas-pwb.py
+++ src/mowas-pwb.py
@@ -18,109 +18,99 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 import logging
-import argparse
-import sys
 import signal
 from modules.mowas import process_mowas_data
-from modules.utils import get_program_config_from_file, signal_term_handler
+from modules.utils import (
+    get_program_config_from_file,
+    signal_term_handler,
+    get_command_line_params,
+)
+from modules.aprsdotfi import get_position_on_aprsfi
 
 # Set up the global logger variable
 logging.basicConfig(
-	level=logging.INFO, format="%(asctime)s %(module)s -%(levelname)s- %(message)s"
+    level=logging.INFO, format="%(asctime)s %(module)s -%(levelname)s- %(message)s"
 )
 logger = logging.getLogger(__name__)
 
 
-
-
 if __name__ == "__main__":
 
+    (
+        mowas_configfile,
+        mowas_test_configuration,
+        mowas_run_interval,
+        mowas_dapnet_destination_callsign,
+        mowas_telegram_destination_id,
+        mowas_disable_telegram,
+        mowas_disable_dapnet,
+        mowas_follow_the_ham,
+        mowas_generate_test_message,
+    ) = get_command_line_params()
 
-	parser = argparse.ArgumentParser()
+    if mowas_disable_dapnet and mowas_disable_telegram:
+        logger.info(
+            msg="User has disabled both output options (DAPNET and Telegram); exiting..."
+        )
+        exit(0)
 
-	parser.add_argument(
-		"--configfile",
-		default="mowas-pwb.cfg",
-		type=argparse.FileType("r"),
-		help="Program config file name",
-	)
+    # get our configuration data from the external configuration file
+    (
+        success,
+        mowas_aprsdotfi_api_key,
+        mowas_dapnet_login_callsign,
+        mowas_dapnet_login_passcode,
+        mowas_watch_areas,
+        mowas_telegram_bot_token,
+    ) = get_program_config_from_file(config_filename=mowas_configfile)
+    if not success:
+        logger.info(msg="Error while parsing the program config file; exiting...")
+        exit(0)
 
-	parser.add_argument(
-		"--test_configuration",
-		dest="test_configuration",
-		action="store_true",
-		help="Checks the program config, sends one test message to the activated channel(s) and then exits the program",
-	)
+    # Define some boolean hints on what is enabled and what is not
+    mowas_aprsdotfi_enabled = (
+        False if mowas_aprsdotfi_api_key == "NOT_CONFIGURED" else True
+    )
+    mowas_dapnet_enabled = (
+        False if mowas_dapnet_login_callsign == "NOT_CONFIGURED" else True
+    )
+    mowas_telegram_enabled = (
+        False if mowas_telegram_bot_token == "NOT_CONFIGURED" else True
+    )
 
-	parser.add_argument(
-		"--disable_dapnet",
-		dest="disable_dapnet",
-		action="store_true",
-		help="Disables any messages to be sent out to DAPNET and disregaards the DAPNET credentials from the cfg file",
-	)
+    # Check if the user asks us to track a ham radio user but has not configured aprs.fi credentials
+    if not mowas_aprsdotfi_enabled and mowas_follow_the_ham:
+        logger.info(
+            msg=f"Cannot track call sign for {mowas_follow_the_ham} - aprs.fi credentials are not configured; exiting..."
+        )
+        exit(0)
 
-	parser.add_argument(
-		"--disable_telegram",
-		dest="disable_telegram",
-		action="store_true",
-		help="Disables any messages to be sent out to Telegram and disregaards the Telegram credentials from the cfg file",
-	)
+    # If the user wants us to track a ham radio user AND has configured
+    # the aprs.fi credentials, verify if we can access aprs.fi and if
+    # that user's call sign can be found via the API
+    if mowas_aprsdotfi_enabled and mowas_follow_the_ham:
+        mowas_follow_the_ham = mowas_follow_the_ham.upper()
+        success, latitude, longitude = get_position_on_aprsfi(
+            aprsfi_callsign=mowas_follow_the_ham,
+            aprsdotfi_api_key=mowas_aprsdotfi_api_key,
+        )
+        if not success:
+            logger.info(
+                msg=f"Cannot find call sign {mowas_follow_the_ham} or aprs.fi access key is misconfigured; exiting ..."
+            )
+            exit(0)
 
-	parser.add_argument(
-		"--run-interval",
-		dest="run_interval",
-		default=30,
-		type=int,
-		help="Run interval (minutes)",
-	)
+    #
+    # We've checked all parameters - let's start with setting up our environment
+    #
 
-	parser.add_argument(
-		"--dapnet-destination-callsign",
-		default="DF1JSL",
-		dest="dapnet_destination_callsign",
-		type=str,
-		help="DAPNET destination call sign which will receive the messages",
-	)
+    # Register the SIGTERM handler; this will allow a safe shutdown of the program
+    logger.info(msg="Registering SIGTERM handler for safe shutdown...")
+    signal.signal(signal.SIGTERM, signal_term_handler)
 
-	parser.add_argument(
-		"--telegram-destination-id",
-		default=0,
-		dest="telegram_destination_id",
-		type=int,
-		help="Telegram user ID (use bot 'UserInfoBot')",
-	)
-
-	parser.set_defaults(add_example_data=False)
-
-	args = parser.parse_args()
-
-	mowas_configfile = args.configfile.name
-	mowas_test_configuration = args.test_configuration
-	mowas_run_interval = args.run_interval
-	mowas_dapnet_destination_callsign = args.dapnet_destination_callsign
-	mowas_telegram_destination_id = args.telegram_destination_id
-	mowas_disable_telegram = args.disable_telegram
-	mowas_disable_dapnet = args.disable_dapnet
-
-	if mowas_disable_dapnet and mowas_disable_telegram:
-		logger.info(msg="User has disabled both output options; exiting...")
-		exit(0)
-
-	success, mowas_aprsdotfi_api_key, mowas_dapnet_login_callsign, mowas_dapnet_login_passcode, mowas_watch_areas = get_program_config_from_file(config_filename=mowas_configfile)
-	if not success:
-		logger.info(msg="Error while parsing the program config file; exiting...")
-		exit(0)
-
-	mowas_aprsdotfi_enabled = False if mowas_aprsdotfi_api_key == "NOT_CONFIGURED" else True
-	mowas_dapnet_enabled = False if mowas_dapnet_login_callsign == "NOT_CONFIGURED" else True
-
-	# Register the SIGTERM handler; this will allow a safe shutdown of the program
-	logger.info(msg="Registering SIGTERM handler for safe shutdown...")
-	signal.signal(signal.SIGTERM, signal_term_handler)
-
-	try:
-		pass
-	except (KeyboardInterrupt, SystemExit):
-		logger.info(
-			msg="KeyboardInterrupt or SystemExit in progress; shutting down ..."
-		)
+    try:
+        pass
+    except (KeyboardInterrupt, SystemExit):
+        logger.info(
+            msg="KeyboardInterrupt or SystemExit in progress; shutting down ..."
+        )
