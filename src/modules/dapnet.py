63040f64dcda20a5f01cc6bb52ae5e4a6e9c1f94
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2021-08-20 11:34:38 +0200

    Added simulation mode and a few comments

--- src/modules/dapnet.py
+++ src/modules/dapnet.py
@@ -39,7 +39,38 @@ def send_dapnet_message(
     dapnet_api_transmitter_group: str = "dl-all",
     dapnet_api_server="http://www.hampager.de:8080/calls",
     dapnet_high_priority_message: bool = False,
+    simulate_send: bool = False,
 ):
+    """
+    Send content to the DAPNET network
+    Parameters
+    ==========
+    to_callsign : 'str'
+        Target Call Sign that will receive the message (SSID content will be removed)
+    message : 'str'
+        Message String that will be sent to the user. May trigger 1..n messages,
+        depending on the message length
+    dapnet_login_callsign: 'str'
+        login user name to the DAPNET API
+    dapnet_login_password: 'str'
+        login password to the DAPNET API
+    dapnet_api_transmitter_group: 'str'
+        Default is 'dl-all' as the program only targets
+        German users. Change this if necessary.
+    dapnet_api_server: 'str'
+        DAPNET API server
+    dapnet_high_priority_message: 'bool'
+        send message as high priority True/False
+    simulate_send: 'bool'
+        Set to true if you want to simulate sending
+        content to the DAPNET network  
+
+    Returns
+    =======
+    success : 'bool'
+            True if operation was successful
+    """
+
     success = False
 
     # Check if we have received a valid MOWAS status
@@ -75,24 +106,28 @@ def send_dapnet_message(
                 "emergency": dapnet_high_priority_message,
             }
             dapnet_payload_json = json.dumps(dapnet_payload)
-            response = requests.post(
-                url=dapnet_api_server,
-                data=dapnet_payload_json,
-                auth=HTTPBasicAuth(
-                    username=dapnet_login_callsign, password=dapnet_login_passcode
-                ),
-            )  # Exception handling einbauen
-            if response.status_code != 201:
-                success = False
+            if not simulate_send:
+                logger.debug(msg="Sending content to DAPNET server")
+                response = requests.post(
+                    url=dapnet_api_server,
+                    data=dapnet_payload_json,
+                    auth=HTTPBasicAuth(
+                        username=dapnet_login_callsign, password=dapnet_login_passcode
+                    ),
+                )  # Exception handling einbauen
+                if response.status_code != 201:
+                    success = False
+                    logger.info(
+                        msg=f"FAILED to send DAPNET message to {dapnet_to_callsign} via '{dapnet_api_transmitter_group}'"
+                    )
+                    return success
                 logger.info(
-                    msg=f"DAPNET message dispatch to {dapnet_to_callsign} via '{dapnet_api_transmitter_group}' failed"
+                    msg=f"Successfully sent message {destination_list.index(destination_message)} of {len(destination_list)-1} to DAPNET"
                 )
-                return success
-            logger.info(
-                msg=f"Successfully sent message {destination_list.index(destination_message)} of {len(destination_list)-1} to DAPNET"
-            )
-            if destination_list.index(destination_message) < len(destination_list) - 1:
-                time.sleep(10.0)
+                if destination_list.index(destination_message) < len(destination_list) - 1:
+                    time.sleep(10.0)
+            else:
+                logger.debug(msg=f"Simulating DAPNET 'Send'; message='{destination_message}'")
         success = True
         logger.info(
             msg="All parts of the message were successfully transmitted to DAPNET"
