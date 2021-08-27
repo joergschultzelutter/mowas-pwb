ddfc56e6e0c52b91721c511f116f85a87fda4986
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2021-08-27 16:17:18 +0200

    new code for the output generator

--- src/modules/outputgenerator.py
+++ src/modules/outputgenerator.py
@@ -18,6 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 import logging
+from modules.utils import convert_text_to_plain_ascii, remove_html_content
 
 # Set up the global logger variable
 logging.basicConfig(
@@ -26,8 +27,20 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def generate_dapnet_messages(mowas_messages_to_send: dict):
-    output_list = []
+
+
+
+def generate_dapnet_messages(mowas_messages_to_send: dict, warncell_data: dict):
+
+    # This is our target list element which contains all messages that
+    # are to be sent to our DAPNET account. One list entry equals one 
+    # logical message along with its DAPNET priority. As DAPNET can only 
+    # digest 80 chars per physical message, the send-to-DAPNET function 
+    # will chop up the messages into multi-message packets, if necessary
+    output_list = {}
+
+    # Iterate through our list of messages
+    # Get all of the information that is associated with our message
     for mowas_message_id in mowas_messages_to_send:
         headline = mowas_messages_to_send[mowas_message_id]["headline"]
         urgency = mowas_messages_to_send[mowas_message_id]["urgency"]
@@ -40,10 +53,33 @@ def generate_dapnet_messages(mowas_messages_to_send: dict):
         geocodes = mowas_messages_to_send[mowas_message_id]["geocodes"]
         dapnet_high_prio = mowas_messages_to_send[mowas_message_id]["dapnet_high_prio"]
 
+        # Construct the outgoing message
+        #
+        # Everything needs to be converted to plain ASCII 7-bit
+        # Message always starts with an indicator whether this is an Alert, Update or Cancel
+        # A: => Alert, U: => Update, C: => Cancel 
+        msg = f"{msgtype[0:1]}:"
+
+        # Remove potential HTML content from msg headline, convert it to ASCII and add it to the message
+        msg = msg + convert_text_to_plain_ascii(remove_html_content(headline)) + " "
+
+        # Remove potential HTML content from msg instructions, convert it to ASCII and add it to the message
+        # Remove potential HTML content from msg headline, convert it to ASCII and add it to the message
+        msg = msg + convert_text_to_plain_ascii(remove_html_content(instruction)) + " "
+
+        # retrieve the affected geocode(s), get the area data and add the short name for that area to the message
+        for geocode in geocodes:
+            if geocode in warncell_data:
+                short_name = warncell_data[geocode]["short_name"]
+                msg = msg + convert_text_to_plain_ascii(short_name) + " "                
+
+        message = {"msg": msg, "priority": dapnet_high_prio}
+        output_list[mowas_message_id] = message
+
     return output_list
 
 
-def generate_telegram_messages(mowas_messages_to_send: dict):
+def generate_telegram_messages(mowas_messages_to_send: dict, warncell_data: dict):
     output_list = []
     for mowas_message_id in mowas_messages_to_send:
         headline = mowas_messages_to_send[mowas_message_id]["headline"]
