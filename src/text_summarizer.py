2cdaa8a15bb8bdbd39568c8637788cbd8026d9c2
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2023-11-07 09:28:06 +0100

    Started work on text summarizer

---
+++ src/text_summarizer.py
@@ -0,0 +1,56 @@
+#
+# mowas-pwb: Text summarizer
+# Author: Joerg Schultze-Lutter, 2023
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Shorten and abbreviate the original input text which - unfortunately - is
+# usually provided by the BBK site in epic proportions. We try to shorten the
+# text as much as possible, thus rendering the output text to a format that is
+# more compatible with e.g. SMS devices.
+#
+# https://github.com/dmmiller612/bert-extractive-summarizer
+#
+from summarizer import Summarizer
+
+
+def summarize_input_text(input_text: str):
+    """
+    Summarize and abbreviate text
+    ==========
+    input_text: 'str'
+        The text that we want to shorten and
+        abbreviate
+    Returns
+    =======
+    response: 'str'
+        Our abbreviated text
+    """
+
+    # Abbreviate and shorten our text
+    model = Summarizer(reduce_option="max")
+    result = model(input_text)
+    response = "".join(result)
+
+    # failsafe in case no content was returned
+    response = input_text if len(response) == 0 else response
+
+    return response
+
+
+# You can run this module if you want to initiate the one-time
+# download of the summarizer modules
+if __name__ == "__main__":
+    print(summarize_input_text("This is a very long text"))
